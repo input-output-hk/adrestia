7a87f2c62bd5e99ca17ad96ffdd29bc69315d5af
KtorZ <matthias.benkort@gmail.com>
2020-10-09 12:27:10 +0200

    add script for running stylish-haskell on a specific version

---
+++ .haskell/stylish-haskell.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# Ported from https://raw.githubusercontent.com/ndmitchell/neil/master/misc/travis.sh
+
+set -e
+
+PACKAGE=stylish-haskell
+REPOSITORY=jaspervdj/$PACKAGE
+
+if [[ -z "${VERSION}" ]]; then
+  echo "VERSION must be provided as ENV var."
+  exit 1
+fi
+
+echo Downloading and running $PACKAGE...
+
+URL=https://github.com/$REPOSITORY/releases/download/$VERSION/$PACKAGE-$VERSION-linux-x86_64.tar.gz
+TEMP=$(mktemp --directory .$PACKAGE-XXXXX)
+
+cleanup(){
+    rm -r $TEMP
+}
+trap cleanup EXIT
+
+echo $URL
+
+curl --progress-bar --location -o$TEMP/$PACKAGE.tar.gz $URL
+tar -xzf $TEMP/$PACKAGE.tar.gz -C$TEMP
+$TEMP/$PACKAGE-$VERSION/$PACKAGE $*
