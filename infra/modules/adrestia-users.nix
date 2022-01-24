2a330f89df8d1b5ee500f405e3161cd64febbe8d
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2022-01-24 19:26:10 +1000

    infra: Add ssh public keys

--- infra/modules/adrestia-users.nix
+++ infra/modules/adrestia-users.nix
@@ -4,9 +4,12 @@
     fullName = "Adrestia";
     extraConfig.hashedPassword = ""; # login locally without a password
     sshKeys = [
+      ../pubkeys/anviking.pub
+      ../pubkeys/id_apfelmus_adrestia.pub
+      ../pubkeys/jonathanknowles.pub
+      ../pubkeys/paweljakubas.pub
       ../pubkeys/rvl.pub
       ../pubkeys/sevanspowell.pub
-      ../pubkeys/anviking.pub
     ];
   };
 }
