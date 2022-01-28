4cf4b1a281448c8b8ccd3fe9f99a50318a6e88bb
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2022-01-28 12:31:28 +1000

    nix: Attempt to fix problem where flake doesn't work on GH actions

--- flake.nix
+++ flake.nix
@@ -2,12 +2,23 @@
   description = "Adrestia Project Tools";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+    # nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+    nixpkgs.url = github:NixOS/nixpkgs/d77bbfcbb650d9c219ca3286e1efb707b922d7c2;
     flake-utils.url = github:numtide/flake-utils;
-    emanote.url = github:srid/emanote;
+    emanote = {
+      url = github:srid/emanote;
+      inputs.ema.follows = "ema";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    ema = {
+      url = github:srid/ema;
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, emanote }:
+  outputs = { self, nixpkgs, flake-utils, emanote, ema }:
     flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system: let
       pkgs = nixpkgs.legacyPackages.${system};
 
