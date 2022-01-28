a286ae9beb7163ff1808f3d143c972eb9c5c013f
Rodney Lorrimar <rodney.lorrimar@iohk.io>
2022-01-28 16:00:45 +1000

    docs: Improve hash links workaround script

--- docs/static/adrestia.js
+++ docs/static/adrestia.js
@@ -1,14 +1,23 @@
 // Keep anchor #links on the same page, when they would otherwise redirect to
 // the page's <base href>. Ref: https://stackoverflow.com/q/8108836
 (function() {
-  function fixLink(el) {
+  function anchorHrefHash(el) {
     if (el.tagName.toLowerCase() === "a") {
       var href = el.getAttribute("href");
       if (href && href.indexOf("#") === 0) {
-        el.href = location.pathname + el.getAttribute("href");
+        return el.getAttribute("href");
       }
     }
   }
+
+  function fixLink(el) {
+    var hash = anchorHrefHash(el);
+    if (hash) {
+      el.href = location.pathname + hash;
+    }
+    return el;
+  }
+
   // Adjust href for all existing links.
   document.addEventListener("DOMContentLoaded", function() {
     const es = document.getElementsByTagName("a");
@@ -16,8 +25,19 @@
       fixLink(es[i]);
     }
   });
-  // Adjust href for dynamically added links - when they are clicked.
-  document.addEventListener("click", function(ev) { fixLink(ev.target); });
+
+  document.addEventListener("click", function(ev) {
+    // Adjust href for dynamically added links - when they are clicked.
+    var el = fixLink(ev.target);
+    // Special case for ema live server: handle the click
+    if (el && !!window.connected) {
+      if (el.hash && el.pathname === window.location.pathname) {
+        window.location.hash = el.hash;
+        ev.stopPropagation();
+        ev.preventDefault();
+      }
+    }
+  });
 })();
 
 function mermaidClick(nodeId) {
